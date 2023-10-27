9a188376eb7be4e75456fd28a06dc5c52c858c8d
Neo319 <alexmnevins@gmail.com>
2023-10-27 16:17:18 -0400

    resolve git error by starting from old branch

--- script.js
+++ script.js
@@ -7,84 +7,106 @@ function getComputerChoice() {
     else return "scissors";
 }
 
-function getPlayerChoice() {
-    let choice = prompt ("enter your choice:");
-    choice = choice.toLowerCase();
 
-    if (choice == "rock" || choice == "paper" || choice == "scissors") {
-        return choice;
-    }
-    else {
-        while (true) {
-            let choice = prompt (`Invalid entry! Enter "rock", "paper" or "scissors":`)
-            choice = choice.toLowerCase();
-            
-            if (choice == "rock" || choice == "paper" || choice == "scissors") {
-                return choice;
-            }
-        }
-    }
- }
+const rock = document.getElementById ("rock");
+const paper = document.getElementById ("paper");
+const scissors = document.getElementById ("scissors");
+const results = document.getElementById ("results");
+const winDisplay = document.getElementById ("winner");
 
- 
-function playRound(comChoice, userChoice) {
-    console.log (`playing user: ${userChoice} com: ${comChoice}`);
-    
-    if (comChoice === userChoice) {
-        return "tie";
-    }
+let playerWins = 0;
+let comWins = 0;
+let ties = 0;
 
-    if  ((comChoice == "rock" && userChoice == "scissors")  ||
+rock.addEventListener ("click", () => {
+    playRound("rock");
+});
+
+paper.addEventListener ("click",  () => {
+    playRound("paper");
+});
+
+scissors.addEventListener ("click", () => {
+    playRound("scissors");
+});
+
+function playRound(playerChoice) {
+    let winner = ''
+    let comChoice = getComputerChoice();
         
-        (comChoice == "paper" && userChoice == "rock")  || 
-    
-        (comChoice == "scissors" && userChoice == "paper")) {
-        return "com wins";
+    if (comChoice === playerChoice) {
+        winner = "tie";
+    }
+    if  ((comChoice == "rock" && playerChoice == "scissors")  ||
+    (comChoice == "paper" && playerChoice == "rock")  || 
+    (comChoice == "scissors" && playerChoice == "paper")) {
+        winner = "com wins";
+    }
+    if  ((playerChoice == "rock" && comChoice == "scissors")  ||
+    (playerChoice == "paper" && comChoice == "rock")  || 
+    (playerChoice == "scissors" && comChoice == "paper")) {
+        winner = "player wins";    
+    }
+    results.textContent = `playing user: ${playerChoice} com: ${comChoice} -- ${winner}`;
+
+    if (winner === "tie") {ties++}
+    else if (winner === "com wins") {comWins++}
+    else if (winner === "player wins") {playerWins++}
+    else console.log("there has been an error...");
+
+    score.textContent = `Score: player = ${playerWins}; com = ${comWins}; ties = ${ties}`;
+
+    if (playerWins == 5) {winDisplay.textContent = "PLAYER WINS"}
+    else if (comWins == 5) {winDisplay.textContent = "COM WINS"};
+
+    if (playerWins == 5 || comWins == 5) {
+        rock.disabled = true;
+        paper.disabled = true;
+        scissors.disabled = true;
     }
 
-    if  ((userChoice == "rock" && comChoice == "scissors")  ||
-
-        (userChoice == "paper" && comChoice == "rock")  || 
-    
-        (userChoice == "scissors" && comChoice == "paper")) {
-        return "player wins"
-    }
 }
 
 
-function game() {
-    console.log("beginning game")
-
-    // CALL VARIABLES FIRST
-    let playerWins = 0;
-    let comWins = 0;
-    let ties = 0;
 
 
-    for (i = 0; i <= 4; i++) {
-        console.log (`round ${i + 1}`)
-        
-        let playerChoice = getPlayerChoice();
-        let computerChoice = getComputerChoice();
 
 
-        let result = playRound(computerChoice, playerChoice);
 
-        if (result == "player wins") {playerWins++;}
-        if (result == "com wins") {comWins++;}
-        if (result == "tie") {ties++;}
 
-        console.log(`round ${i+1} result: ${result}`);
-        console.log(`player score: ${playerWins}`);
-        console.log(`com score: ${comWins}`);
-        console.log(`ties: ${ties}`);
-    }
+// function game() {
+//     console.log("beginning game")
 
-    if (playerWins > comWins) {console.log("Player Wins!!")}
-    else if (comWins > playerWins) {console.log("Computer Wins!!")}
-    else if (playerWins == comWins) {console.log("Draw!!")}
-    else {console.log("there has been an error...")}
+//     // CALL VARIABLES FIRST
+//     let playerWins = 0;
+//     let comWins = 0;
+//     let ties = 0;
 
-}
 
-game();
+//     for (i = 0; i <= 4; i++) {
+//         console.log (`round ${i + 1}`)
+
+//         let playerChoice = getPlayerChoice();
+//         let computerChoice = getComputerChoice();
+
+
+//         let roundResult = playRound(computerChoice, playerChoice);
+
+//         if (roundResult == "player wins") {playerWins++;}
+//         if (roundResult == "com wins") {comWins++;}
+//         if (roundResult == "tie") {ties++;}
+
+//         console.log(`round ${i+1} roundResult: ${roundResult}`);
+//         console.log(`player score: ${playerWins}`);
+//         console.log(`com score: ${comWins}`);
+//         console.log(`ties: ${ties}`);
+//     }
+
+//     if (playerWins > comWins) {console.log("Player Wins!!")}
+//     else if (comWins > playerWins) {console.log("Computer Wins!!")}
+//     else if (playerWins == comWins) {console.log("Draw!!")}
+//     else {console.log("there has been an error...")}
+
+// }
+
+// game();
